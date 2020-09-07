f124a9c1f93b724e1aef282f5b18ee8b9f9e7533
wormholer <cheng.wang@kyligence.io>
2020-09-07 13:51:37 +0800

    generate data locally

--- bin/run.sh
+++ bin/run.sh
@@ -56,73 +56,19 @@ if [ ${scale2int} -gt 1000 ]; then
     exit
 fi
 
-# check for existence of hadoop streaming
-if [ -n "$HADOOP_HOME" ]; then
-    # for hadoop 1.0.x
-    if [ -z "$HADOOP_STREAMING_JAR" ] && [ -e $HADOOP_HOME/contrib/streaming/hadoop-streaming-*.jar ]; then
-        HADOOP_STREAMING_JAR=$HADOOP_HOME/contrib/streaming/hadoop-streaming-*.jar
-    fi
-    # for hadoop 2.0.x
-    if [ -z "$HADOOP_STREAMING_JAR" ] && [ -e $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar ]; then
-        HADOOP_STREAMING_JAR=$HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-*.jar
-    fi
-    # for other hadoop version
-    if [ -z "$HADOOP_STREAMING_JAR" ]; then
-        HADOOP_STREAMING_JAR=`find $HADOOP_HOME -name hadoop-stream*.jar -type f`
-    fi
-else
-    # for hadoop 1.0.x
-    if [ -z "$HADOOP_STREAMING_JAR" ] && [ -e `dirname ${HADOOP_EXAMPLES_JAR}`/contrib/streaming/hadoop-streaming-*.jar ]; then
-        HADOOP_STREAMING_JAR=`dirname ${HADOOP_EXAMPLES_JAR}`/contrib/streaming/hadoop-streaming-*.jar
-    fi
-    # for hadoop 2.0.x
-    if [ -z "$HADOOP_STREAMING_JAR" ] && [ -e `dirname ${HADOOP_EXAMPLES_JAR}`/../tools/lib/hadoop-streaming-*.jar ]; then
-        HADOOP_STREAMING_JAR=`dirname ${HADOOP_EXAMPLES_JAR}`/../tools/lib/hadoop-streaming-*.jar
-    fi
-fi
-
-if [ -z "$HADOOP_STREAMING_JAR" ]; then
-    echo 'Can not find hadoop-streaming jar file, please set HADOOP_STREAMING_JAR path.'
-    exit
-fi
-
-# clean up temp files
-rm -f $LOCAL_TMP_DIR/ssb/input/*
-mkdir -p $LOCAL_TMP_DIR/ssb/input
-for c in $(seq $PARALLEL_TASKS);do
-    echo "${c}" > $LOCAL_TMP_DIR/ssb/input/$c.txt
-    echo "${HDFS_BASE_DIR}" >> $LOCAL_TMP_DIR/ssb/input/$c.txt
-    echo "${scale}" >> $LOCAL_TMP_DIR/ssb/input/$c.txt
-    echo "${PARALLEL_TASKS}" >> $LOCAL_TMP_DIR/ssb/input/$c.txt
-done
-
 # clean up hive metadata
 hive -e "DROP DATABASE IF EXISTS ${database} CASCADE;"
 
 # clean up previous data if available
 hadoop fs -rm -r $HDFS_BASE_DIR
 hadoop fs -mkdir -p $HDFS_BASE_DIR
-hadoop fs -mkdir -p $HDFS_BASE_DIR/tmp
-hadoop fs -moveFromLocal $LOCAL_TMP_DIR/ssb/input $HDFS_BASE_DIR/tmp
 hadoop fs -mkdir -p $HDFS_BASE_DIR/data
+
 for tbl in customer part supplier date lineorder
 do
     hadoop fs -mkdir -p $HDFS_BASE_DIR/data/$tbl
     hadoop fs -chmod 777 $HDFS_BASE_DIR/data/$tbl
 done
-
-# run hadoop streaming job
-OPTION="-D mapred.reduce.tasks=0 \
--D mapred.job.name=generate_ssb_date \
--D mapred.task.timeout=0 \
--input ${HDFS_BASE_DIR}/tmp/input \
--output ${HDFS_BASE_DIR}/data/output \
--mapper ${dir}/dbgen.sh \
--file ${dir}/dbgen.sh -file ${dir}/ssb.conf -file ${ssb_home}/dbgen -file ${ssb_home}/dists.dss"
-
-echo hadoop jar ${HADOOP_STREAMING_JAR} ${OPTION}
-
-hadoop jar ${HADOOP_STREAMING_JAR} ${OPTION}
 
 echo "Creating Hive External Tables"
 ls $dir/../hive/* | xargs -I {} cp {} {}.tmp
@@ -138,3 +84,50 @@ else
     hive -f $dir/../hive/2_create_views.sql.tmp
 fi
 rm $dir/../hive/*.tmp
+
+
+# run datagen locally
+cp ${ssb_home}/dbgen ${dir}
+if [[ "$?" -ne 0 ]]
+   then
+   echo "dbgen is expected, please make sure dbgen has been made"
+fi
+cp ${ssb_home}/dists.dss ${dir}
+if [[ "$?" -ne 0 ]]
+   then
+   echo "dists.dss is expected, please make sure dists.dss has been made"
+fi
+
+for ((task=0; task <= ${PARALLEL_TASKS}; task++))
+do
+    if [ ${task} == 0 ];then
+        for tbl in customer part supplier date
+        do
+            #echo "Begin generating - $tbl"
+            #echo "./dbgen -s ${SCALE} -T ${tbl}"
+            ${dir}/dbgen -q -s ${scale} -c ./ssb.conf -T ${tbl}
+            #echo "Finish generating and start to upload - ${tbl}"
+            whoami
+            hadoop fs -put ${tbl}.tbl* ${HDFS_BASE_DIR}/data/$tbl/
+            if [[ "$?" -ne 0 ]]
+            then
+                echo "retrying hdfs put"
+                hadoop fs -put ${tbl}.tbl* ${HDFS_BASE_DIR}/data/$tbl/
+            fi
+            rm -f ${tbl}.tbl* || exit 0
+            #echo "Upload finished - $tbl"
+        done
+    else
+        for tbl in lineorder
+        do
+            #echo "Begin generating - $tbl"
+            #echo "./dbgen -C $((PARALLEL_TASKS-1)) -S $((TASK_NUM-1)) -s ${SCALE} -T ${tbl}"
+            ${dir}/dbgen -q -c ./ssb.conf -C $((PARALLEL_TASKS-1)) -S $((task)) -s ${scale} -T ${tbl}
+            #echo "Finish generating and start to upload - ${tbl}"
+            hadoop fs -put ${tbl}.tbl* ${HDFS_BASE_DIR}/data/$tbl/
+            rm -f ${tbl}.tbl* || exit 0
+            #echo "Upload finished - $tbl"
+        done
+    fi
+done
+
